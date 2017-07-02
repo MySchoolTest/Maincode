03b082e8b8b5c461c1f62584357fa336611eda0e
samiran91 <samiransen.it@gmail.com>
2017-07-02 17:46:02 +0530

    My School main codebase added

---
+++ Code/MySchool/MySchool/Models/TestModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MySchool.Models
+{
+    public class TestModel
+    {
+        public string Message { get; set; }
+    }
+}
